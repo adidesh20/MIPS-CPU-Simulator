c6699ae8b0803cdc821850ab000e94573faddc30
Aditya <aydeshpande20@gmail.com>
2019-11-04 22:16:54 +0000

    Data memory added

--- src/memory.hpp
+++ src/memory.hpp
@@ -1,20 +1,25 @@
-#ifndef MEMORY
+#ifndef MEMORY_HEAD
 
-#define MEMORY
+#define MEMORY_HEAD
 
 #include <iostream>
 #include <vector>
 #include <string>
+#include "constants.hpp"
 
 class memory
 {
 private:
 	std::vector<uint8_t> ADDR_INSTR;
+	std::vector<uint8_t> ADDR_DATA;
 	
 	//space for other memories to be defined in the future
 
 public:
 	memory(std::ifstream &bin);
+	
+	int get_instr(int &pc, uint32_t& instr);
+	
 };
 
 #endif
