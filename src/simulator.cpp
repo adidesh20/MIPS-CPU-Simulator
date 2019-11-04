f90f7295c3ab611896c0d8656e69f4ece0716290
Aditya <aydeshpande20@gmail.com>
2019-11-04 03:05:14 +0000

    Simulator can now read binary and store it in ADDR_INSTR vector

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -3,8 +3,8 @@
 #include <fstream>
 #include <cstdlib>
 
-//#include "memory.hpp"
-//#include "memory.cpp"
+#include "memory.hpp"
+#include "memory.cpp"
 
 int main(int argc, char *argv[])
 {
@@ -20,8 +20,8 @@ int main(int argc, char *argv[])
 		return(-21);
 	}
 
-	std::ifstream bin(binName.c_str, std::ios::in | std::ios::binary | std::ios::ate);
-	//bin.open(binFile.c_str(), std::ios::in | std::ios::binary | std::ios::ate);
+	std::ifstream bin(argv[1], std::ios::in | std::ios::binary | std::ios::ate);
+
 	
 	if (!bin.is_open())
 	{
@@ -31,8 +31,38 @@ int main(int argc, char *argv[])
 	else
 	{
 		std::cerr << "File was found!";
-		return 0;
+		
 	}
+	
+	//reading the binary and storing in a vector
+	//should happen in a memory class which will be written later
+	bin.seekg(0, bin.end);
+	int size = bin.tellg();
+	bin.seekg(0);
+	char buffer[size];
+	bin.read(buffer, size);
+
+	std::vector<uint8_t> ADDR_INSTR;
+	ADDR_INSTR.assign(0x1000000, 0);
+	
+	for (int i = 0; i < size/8; i++)
+	{
+		std::string byte;
+		for (int j = 0; j < 8; j++)
+		{
+			byte.push_back(buffer[j + (i * 8)]);
+		}
+		//std::cout << std::endl << byte << std::endl;
+		uint8_t int_byte = std::stoi(byte, 0, 2);
+		ADDR_INSTR[i] = int_byte;
+	}
+
+
+	
+	
+
+
+	
 
 	 
 
