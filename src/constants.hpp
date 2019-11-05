c32e0a948c7eedeca1ba66b21cbae24e7b8306bd
Aditya <aydeshpande20@gmail.com>
2019-11-05 15:45:24 +0000

    Header file containing constants to be used accross the project

---
+++ src/constants.hpp
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS
+#define CONSTANTS
+
+#define	INSTR_BASE 0x10000000
+#define  INSTR_LENGTH 0x1000000
+#define  DATA_BASE 0x20000000
+#define  DATA_LENGTH 0x4000000
+#define  GETC_BASE 0x30000000
+#define  GETC_LENGTH 0x4
+#define  PUTC_BASE 0x30000004
+#define  PUTC_LENGTH 0x4
+
+#endif // !CONSTANTS
+
