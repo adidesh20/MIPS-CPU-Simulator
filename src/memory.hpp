5ade5865ae4293a50cb289594c3adce1f97298f9
Aditya <aydeshpande20@gmail.com>
2019-11-08 15:28:25 +0000

    Header file for memory class

--- src/memory.hpp
+++ src/memory.hpp
@@ -6,19 +6,26 @@
 #include <vector>
 #include <string>
 #include "constants.hpp"
+#include "global_vars.hpp"
 
 class memory
 {
 private:
 	std::vector<uint8_t> ADDR_INSTR;
 	std::vector<uint8_t> ADDR_DATA;
-	
 	//space for other memories to be defined in the future
+
+
+	int end_index;
 
 public:
 	memory(std::ifstream &bin);
-	
-	int get_instr(int &pc, uint32_t& instr);
+	void get_instr(uint32_t& instr);
+	void get_byte(uint32_t address, uint8_t& byte);
+	void get_word(uint32_t address, uint32_t& word);
+	void put_byte(uint32_t address, uint8_t& byte);
+	void put_word(uint32_t address, uint32_t& word);
+	bool end_check();
 	
 };
 
