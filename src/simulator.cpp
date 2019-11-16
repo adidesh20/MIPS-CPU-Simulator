331ff4e3b919c58e73350f9acd9938847be1e10f
Aditya <aydeshpande20@gmail.com>
2019-11-16 02:00:06 +0000

    Fixed binary read, added support for multiple instructions, implemented framework for jump delays

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -7,7 +7,7 @@
 #include "memory.hpp"
 #include "instruction.hpp"
 #include "global_vars.hpp"
- 
+
 int main(int argc, char *argv[])
 {
 	//READING AND STORING BINARY FILE
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
 	std::vector<int32_t> registers;
 	registers.assign(32, 0);
-
+	pc = INSTR_BASE;
 	std::ifstream bin(argv[1], std::ios::in | std::ios::binary | std::ios::ate);
 
 
@@ -39,38 +39,35 @@ int main(int argc, char *argv[])
 
 	}
 
+	pc = INSTR_BASE;
+
 	memory mips_mem(bin);
+	uint32_t delay_instruction;
+	instruction_class mips_delay;
 	while (pc != 0)
 	{
 		uint32_t instruction;
 		mips_mem.get_instr(instruction);
-		std::cout << instruction << std::endl;
 		instruction_class mips_instr(instruction);
 		mips_instr.decode();
 		mips_instr.execute(registers, mips_mem);
-
+		pc += 4;
 		if (jump)
 		{
-			pc += 4;
-			uint32_t extra_instruction;
-			instruction_class extra(extra_instruction);
-			extra.decode();
-			extra.execute(registers, mips_mem);
-			pc = jump_addr;
+			if (jump_addr == 0)
+			{
+				int32_t r2 = registers[2] << 24;
+				uint8_t ret = r2 >> 24;
+				std::exit(ret);
+				return 0;
+			}
 			jump = false;
-		}
-		else
-		{
-			pc += 4;
-		}
-		
-		if (mips_mem.end_check())
-		{
-			pc = 0;
+			mips_mem.get_instr(delay_instruction);
+			mips_delay.delayed_init(delay_instruction);
+			mips_delay.decode();
+			mips_delay.execute(registers, mips_mem);
+			pc = jump_addr;
 		}
 	}
-	int32_t r2 = registers[2] << 24;
-	uint8_t ret = r2 >> 24;
-	std::exit(ret);
-	return 0;
+
 }
