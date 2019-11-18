7f1ede9fd01e170ca4f01b6e86f9c1443d5186c7
Aditya <aydeshpande20@gmail.com>
2019-11-18 20:37:17 +0000

    Added hi and lo registers

--- src/global_vars.cpp
+++ src/global_vars.cpp
@@ -1,7 +1,10 @@
+#include "pch.h"
 #include "global_vars.hpp"
 #include "constants.hpp"
 #include <iostream>
 
-int pc;
+uint32_t pc;
 bool jump = false;
 uint32_t jump_addr;
+int32_t reg_hi;
+int32_t reg_lo;
