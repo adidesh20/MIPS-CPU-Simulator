b01d84049a5166611833745853cf9156f3152e90
Aditya <aydeshpande20@gmail.com>
2019-11-20 19:36:02 +0000

    Fixed sign extentions for get and put word/halfword/byte

--- src/memory.hpp
+++ src/memory.hpp
@@ -22,11 +22,12 @@ public:
 
 	memory(std::ifstream &bin);
 	void get_instr(uint32_t& instr);
-	void get_byte(uint32_t address, uint8_t& byte);
+	void get_byte(uint32_t address, int8_t& byte);
 	void get_word(uint32_t address, uint32_t& word);
-	void put_byte(uint32_t address, uint8_t& byte);
+	void get_half_word(uint32_t address, int16_t& half_word);
+	void put_byte(uint32_t address, int8_t& byte);
 	void put_word(uint32_t address, uint32_t& word);
-	bool end_check();
+	void put_half_word(uint32_t address, int16_t& half_word);
 
 };
 
