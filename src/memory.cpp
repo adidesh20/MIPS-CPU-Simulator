31d75bad1c91631a90fbb13c60acadc1116e8d49
Aditya <aydeshpande20@gmail.com>
2019-11-25 20:08:15 +0000

    Commit of complete memory class

--- src/memory.cpp
+++ src/memory.cpp
@@ -10,10 +10,10 @@ memory::memory(std::ifstream &bin)
 {
 	bin.seekg(0, bin.end);
 	int size = bin.tellg();
-	if (size % 4 != 0)
-	{
-		std::exit(-12);
-	}
+    if(size % 4 != 0)
+    {
+        std::exit(-12);
+    }
 	bin.seekg(0);
 	char * buffer;
 	buffer = new char[size];
@@ -27,18 +27,7 @@ memory::memory(std::ifstream &bin)
 	{
 		ADDR_INSTR[i] = buffer[i];
 	}
-	/*for (int i = 0; i < size / 8; i++)
-	{
-		std::string byte;
-		for (int j = 0; j < 8; j++)
-		{
-			byte.push_back(buffer[j + (i * 8)]);
-		}
-		//std::cout << std::endl << byte << std::endl;
-		uint8_t int_byte = std::stoi(byte, 0, 2);
-		ADDR_INSTR[i] = int_byte;
-	}
-	end_index = size / 8;*/
+	
 }
 
 void  memory::get_instr(uint32_t& instr)
