bd399a1f6ecbec9651888779e55a26b3f10d8870
Aditya Deshpande <aydeshpande20@gmail.com>
2020-07-06 03:23:53 +0100

    Moved header files to include

---
+++ include/global_vars.hpp
@@ -0,0 +1,13 @@
+#ifndef GLOBAL
+#define GLOBAL
+#include <iostream>
+
+extern uint32_t pc;
+extern bool jump;
+extern uint32_t jump_addr;
+extern int32_t reg_hi;
+extern int32_t reg_lo;
+
+
+
+#endif
